73c3006c0e31e910a091cb8e941890da99f7ee92
carlosjohn1215@gmail.com <carlosjohn1215@gmail.com>
2024-04-14 19:47:00 +0900

    9-1 3-3

--- database/migrations/2024_04_14_182403_create_posts_table.php
+++ database/migrations/2024_04_14_182403_create_posts_table.php
@@ -6,18 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('body', 200);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('image', 100)->nullable();
+        });
+    }
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  
 
     /**
      * Reverse the migrations.
